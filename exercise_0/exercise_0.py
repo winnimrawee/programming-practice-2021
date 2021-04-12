bb1b6279297f5cf14dd98a8596a7fe4c79e84da7
winnimrawee <82361858+winnimrawee@users.noreply.github.com>
2021-04-12 15:23:35 +0900

    Update exercise_0.py

--- exercise_0/exercise_0.py
+++ exercise_0/exercise_0.py
@@ -3,6 +3,9 @@ def exercise_0(inputs): # DO NOT CHANGE THIS LINE
     This functions receives the input in the parameter 'inputs'. 
     Change the code, so that the output is sqaure of the given input.
     """
+    
+   
+ 
 
     output = inputs
 
