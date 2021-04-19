7ab8ee22f88014f5777cb1c43585218a0cad479f
Shashank Kotyan <shashankkotyan@gmail.com>
2021-04-19 14:33:05 +0900

    Update Exercise 1

---
+++ exercise_1/exercise_1.py
@@ -0,0 +1,9 @@
+def exercise_1(inputs): # DO NOT CHANGE THIS LINE
+    """
+    This functions receives the input in the parameter 'inputs'. 
+    Change the code, so that the output is sqaure of the given input.
+    """
+
+    output = inputs
+
+    return output       # DO NOT CHANGE THIS LINE
