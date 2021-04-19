ae97a57620dd8fbe52ec47c5c4ac27632e367a04
Shashank Kotyan <shashankkotyan@gmail.com>
2021-04-19 15:48:35 +0900

    Updated Output Information

--- exercise_1/exercise_1.py
+++ exercise_1/exercise_1.py
@@ -7,9 +7,23 @@ def exercise_1(inputs): # DO NOT CHANGE THIS LINE
     
     p => ['t101', 't102', 't103']
     q => ['s101', 's102', 's103']
-    r => {'l101':['t101': ['s101', 's102']], 'l102': ['s101', 's102', 's103']]
-    """
-
+    r => {
+        'l101':['t101': ['s101', 's102']], 
+        'l102': ['s101', 's102', 's103']]
+    }
+    p = Person()
+    s = Student()
+    t = Teacher()
+    l = Lecture()
+    
+    output = {
+        1: [Person, p], 
+        2: [Teacher, Student, t, s], 
+        3: [Lecture, l], 
+        4: [Lecture, l], 
+        5: [Lecture, Student, l, s], 
+        6: [Teacher, Student, t, s]
+    }
     output = inputs
 
     return output       # DO NOT CHANGE THIS LINE
