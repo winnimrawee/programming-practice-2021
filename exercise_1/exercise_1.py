ede60175ceaecd6d8ae93f35ab5842e993955ecc
Shashank Kotyan <shashankkotyan@gmail.com>
2021-04-19 15:41:55 +0900

    Updated Information

--- exercise_1/exercise_1.py
+++ exercise_1/exercise_1.py
@@ -2,6 +2,12 @@ def exercise_1(inputs): # DO NOT CHANGE THIS LINE
     """
     This functions receives the input in the parameter 'inputs'. 
     Change the code, so that the output is sqaure of the given input.
+    
+    p, q, r = inputs
+    
+    p => ['t101', 't102', 't103']
+    q => ['s101', 's102', 's103']
+    r => {'l101':['t101': ['s101', 's102']], 'l102': ['s101', 's102', 's103']]
     """
 
     output = inputs
