6853648b0a54f8421696189085ae024a8acc797a
Shashank Kotyan <shashankkotyan@gmail.com>
2021-04-26 13:07:45 +0900

    Update

---
+++ exercise_1/exercise_1.py
@@ -0,0 +1,30 @@
+def exercise_1(inputs): # DO NOT CHANGE THIS LINE
+    """
+    This functions receives the input in the parameter 'inputs'. 
+    Change the code, so that the output is sqaure of the given input.
+    
+    p, q, r = inputs
+    
+    p => ['t101', 't102', 't103']
+    q => ['s101', 's102', 's103']
+    r => {
+        'l101':['t101': ['s101', 's102']], 
+        'l102': ['s101', 's102', 's103']]
+    }
+    p = Person()
+    s = Student()
+    t = Teacher()
+    l = Lecture()
+    
+    output = {
+        1: [Person, p], 
+        2: [Teacher, Student, t, s], 
+        3: [Lecture, l], 
+        4: [Lecture, l], 
+        5: [Lecture, Student, l, s], 
+        6: [Teacher, Student, t, s]
+    }
+    """
+    output = inputs
+
+    return output       # DO NOT CHANGE THIS LINE
