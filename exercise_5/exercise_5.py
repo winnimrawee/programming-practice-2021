dec1df636880e1938d0c1ac5b02f563103eae502
Shashank Kotyan <shashankkotyan@gmail.com>
2021-05-31 15:15:37 +0900

    Update

---
+++ exercise_5/exercise_5.py
@@ -0,0 +1,10 @@
+def exercise_5(inputs): # DO NOT CHANGE THIS LINE
+    """
+    This functions receives the input in the parameter 'inputs'. 
+    Change the code, so that the output is sqaure of the given input.
+
+    Output should be the name of the class.
+    """
+    output = inputs
+
+    return output       # DO NOT CHANGE THIS LINE
